b6f84a1d4f25520200371a11e9abb703c6fad558
Dejan Petrović <dekip@beotel.rs>
2021-08-09 19:42:33 +0200

    Update code.py

--- CircuitPython Examples/code.py
+++ CircuitPython Examples/code.py
@@ -1,8 +1,9 @@
+# Code for reading button state on digital pin 40 (IO40)
 import time
 import board
 from digitalio import DigitalInOut, Direction, Pull
 
-btn = DigitalInOut(board.SWITCH)
+btn = DigitalInOut(board.IO40)
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
 
@@ -10,7 +11,7 @@ while True:
     if not btn.value:
         print("BTN is down")
     else:
-        #print("BTN is up")
+        print("BTN is up")
         pass
 
     time.sleep(0.1) # sleep for debounce
